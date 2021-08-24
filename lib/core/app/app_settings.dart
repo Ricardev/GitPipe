6891a903dc2aa4347f9e6d4b720febea751b48d9
Ricardo Alexandre <72095541+Ricardev@users.noreply.github.com>
2021-08-24 12:13:24 -0300

    Changing the settings

--- lib/core/app/app_settings.dart
+++ lib/core/app/app_settings.dart
@@ -6,15 +6,14 @@ class GitPipe {
 
   static final state = _randomState;
 
-  static final keyApplicationId = 'B6xpYRc71CbH8jKFV0hBIwVwdkIUWbvLrhm2ScV3';
-  static final keyClientKey = 'sDtaPCiYRQ9n30M3ci3pTC3yw464KYjgsXJpYaNO';
-  static final keyParseServerUrl = 'https://parseapi.back4app.com';
+  static final keyApplicationId = 'your App Id';
+  static final keyClientKey = 'your App Client Key';
   static final secret =
-      'a5962e25cf432474577f515b309d0551f04ef03b3f8c53ff8a1962ab67b7c4cc';
+      'your app secret';
   static final appIdentifier =
-      'e0013359b4bc89d6d7c66163791fe3524e376025916a1f4acf7500f569b27223';
-  static final redirectUrl = 'https://parseapi.back4app.com/gitpipe';
+      'your app Id ( one Id is from the back4App and the other one is from GitLab. Only the id from GitLab is necessary, but i want to join both later )';
+  static final redirectUrl = 'the redirect Url';
   static final credentialsFile = File('~/.myapp/credentials.json');
-  static final accessTokenEndpoint = 'https://gitlab.com/oauth/token';
-  static final authorizeEndpoint = 'https://gitlab.com/oauth/authorize';
+  static final accessTokenEndpoint = 'your endpoint';
+  static final authorizeEndpoint = 'your authorize endpoint';
 }
